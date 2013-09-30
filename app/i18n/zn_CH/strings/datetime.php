1b4b745e0bd31ab236ce031f8ea2a0b778b91146
Frederic G. Østby <frederic.g.ostby@gmail.com>
2013-09-30 09:47:55 +0200

    Added Chinese language pack

---
+++ app/i18n/zn_CH/strings/datetime.php
@@ -0,0 +1,21 @@
+<?php
+
+//---------------------------------------------
+// Language file used by mako\DateTime
+//---------------------------------------------
+
+return array
+(
+	'today'       => '今天',
+	'yesterday'   => '昨天',
+	'minute_ago'  => '一分钟前',
+	'minutes_ago' => '%u分钟前',
+	'hour_ago'    => '一小时前',
+	'hours_ago'   => '%u小时前',
+	'day_ago'     => '一天前',
+	'days_ago'    => '%u天前',
+	'week_ago'    => '一周前',
+	'weeks_ago'   => '%u周前',
+);
+
+/** -------------------- End of file --------------------**/
