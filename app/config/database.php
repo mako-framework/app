6f1b5d8a9da6d5ebb525a02bdb7dae5f9535e206
Frederic G. Østby <frederic.g.ostby@gmail.com>
2013-10-08 14:14:33 +0200

    Updated database config

--- app/config/database.php
+++ app/config/database.php
@@ -11,32 +11,28 @@ return array
 	 */
 	
 	'default' => 'test',
-
-	/**
-	 * Enable the query log?
-	 */
-
-	'log_queries' => false,
 	
 	/**
 	 * You can define as many database configurations as you want.
 	 *
-	 * dsn       : PDO data source name
-	 * username  : (optional) Username of the database server
-	 * password  : (optional) Password of the database server
-	 * persistent: (optional) Set to true to make the connection persistent
-	 * queries   : (optional) Queries that will be executed right after a connection has been made
+	 * dsn        : PDO data source name
+	 * username   : (optional) Username of the database server
+	 * password   : (optional) Password of the database server
+	 * persistent : (optional) Set to true to make the connection persistent
+	 * log_queries: (optional) Enable query logging?
+	 * queries    : (optional) Queries that will be executed right after a connection has been made
 	 */
 	
 	'configurations' => array
 	(
 		'test' => array
 		(
-			'dsn'        => 'mysql:dbname=test;host=localhost;port=3306',
-			'username'   => 'username',
-			'password'   => 'password',
-			'persistent' => false,
-			'queries'    => array
+			'dsn'         => 'mysql:dbname=test;host=localhost;port=3306',
+			'username'    => 'username',
+			'password'    => 'password',
+			'persistent'  => false,
+			'log_queries' => false,
+			'queries'     => array
 			(
 				"SET NAMES UTF8",
 			),
@@ -44,8 +40,9 @@ return array
 
 		'sqlite' => array
 		(
-			'dsn'     => 'sqlite:' . MAKO_APPLICATION_PATH . '/storage/database/test.sqlite',
-			'queries' => array
+			'dsn'         => 'sqlite:' . MAKO_APPLICATION_PATH . '/storage/database/test.sqlite',
+			'log_queries' => false,
+			'queries'     => array
 			(
 				"PRAGMA encoding = 'UTF-8'",
 			),
