fcd447d2477406945c052088322324a917ddb760
Frederic G. Østby <frederic.g.ostby@gmail.com>
2014-07-23 17:36:01 +0200

    Added config setting for language cache

--- app/config/application.php
+++ app/config/application.php
@@ -49,6 +49,16 @@ return
 		//'no' => 'nb_NO',
 		//'fr' => 'fr_FR',
 	],
+
+	/**
+	 * Enabling language caching can speed up applications with a lot of language files by
+	 * reducing the number of files it has to load on every request.
+	 * 
+	 * Use the default cache store by setting the config value to TRUE and choose a specific cache configuration 
+	 * by specifing its name (as specified in the cache configuration).
+	 */
+
+	'language_cache' => false,
 	
 	/**
 	 * Locale settings.
