d53787ffffe334ddbabb00e07161ebbaea6d7b7f
Frederic G. Østby <frederic.g.ostby@gmail.com>
2016-07-27 11:44:46 +0200

    Removed MCrypt

--- app/config/crypto.php
+++ app/config/crypto.php
@@ -19,24 +19,15 @@ return
 	 *
 	 * You can define as many cryptography configurations as you want.
 	 *
-	 * The supported cryptography libraries are: "mcrypt", and "openssl".
+	 * The supported cryptography libraries are: "openssl".
 	 *
 	 * library: Cryptography library you want to use.
 	 * cipher : The cipher method to use for encryption.
 	 * key    : Key used to encrypt/decrypt data. You should NOT use the key included with the framework in a production environment!
-	 * mode   : Encryption mode (only required when using the "mcrypt" library).
 	 */
 
 	'configurations' =>
 	[
-		'mcrypt' =>
-		[
-			'library' => 'mcrypt',
-			'cipher'  => MCRYPT_RIJNDAEL_256,
-			'key'     => '`F0=nYsPkxolnlyc+z6jcnRdulJEOfqIyMWwlxeYtnFPi[lKMb',
-			'mode'    => MCRYPT_MODE_CBC,
-		],
-
 		'openssl' =>
 		[
 			'library'  => 'openssl',
