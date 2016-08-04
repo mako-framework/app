61ca3242b082ce7b0ed44cf122acffe432e5277f
Frederic G. Østby <frederic.g.ostby@gmail.com>
2016-08-04 18:12:33 +0200

    Changed default encryption cipher

--- app/config/crypto.php
+++ app/config/crypto.php
@@ -31,8 +31,8 @@ return
 		'openssl' =>
 		[
 			'library'  => 'openssl',
-			'cipher'   => 'AES-256-OFB',
-			'key'      => '`F0=nYsPkxolnlyc+z6jcnRdulJEOfqIyMWwlxeYtnFPi[lKMb',
+			'cipher'   => 'AES-256-CTR',
+			'key'      => 'hex:3462f179ba7b6606a60bbfa160bc7ff857ee91ad7850c97562229dc7228e5e49',
 		],
 	],
 ];
