0bee17f67d898d0e2e76b62413705b3e06dc9d2a
Frederic G. Østby <frederic.g.ostby@gmail.com>
2016-08-10 12:18:52 +0200

    Added persistent connection option to Redis config file

--- app/config/redis.php
+++ app/config/redis.php
@@ -19,36 +19,40 @@ return
 	 *
 	 * You can define as many Redis configurations as you want.
 	 *
-	 * host    : IP address or hostname of the Redis server.
-	 * port    : Port of the Redis server.
-	 * password: (optional) Password of the Redis server.
-	 * database: (optional) Database index to use for the connection.
+	 * host      : IP address or hostname of the Redis server.
+	 * port      : Port of the Redis server.
+	 * persistent: (optional) Set to true to make the connection persistent.
+	 * password  : (optional) Password of the Redis server.
+	 * database  : (optional) Database index to use for the connection.
 	 */
 
 	'configurations' =>
 	[
 		'localhost' =>
 		[
-			'host'     => 'localhost',
-			'port'     => 6379,
-			'password' => '',
-			'database' => 0,
+			'host'       => 'localhost',
+			'port'       => 6379,
+			'persistent' => false,
+			'password'   => '',
+			'database'   => 0,
 		],
 
 		'cache' =>
 		[
-			'host'     => 'localhost',
-			'port'     => 6379,
-			'password' => '',
-			'database' => 1,
+			'host'       => 'localhost',
+			'port'       => 6379,
+			'persistent' => false,
+			'password'   => '',
+			'database'   => 1,
 		],
 
 		'session' =>
 		[
-			'host'     => 'localhost',
-			'port'     => 6379,
-			'password' => '',
-			'database' => 2,
+			'host'       => 'localhost',
+			'port'       => 6379,
+			'persistent' => false,
+			'password'   => '',
+			'database'   => 2,
 		],
 	]
 ];
