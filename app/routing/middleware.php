140ec658d8b7a314ab3362e7ceeca6ec5501d0b1
Frederic G. Østby <frederic.g.ostby@gmail.com>
2019-10-12 20:06:49 +0200

    Update middleware.php

--- app/routing/middleware.php
+++ app/routing/middleware.php
@@ -1,7 +1,9 @@
 <?php
 
+use mako\http\routing\middleware\ContentSecurityPolicy;
 use mako\http\routing\middleware\SecurityHeaders;
 
 /** @var \mako\http\routing\Dispatcher $dispatcher */
 
+//$dispatcher->registerMiddleware('content_security_policy', ContentSecurityPolicy::class);
 //$dispatcher->registerMiddleware('security_headers', SecurityHeaders::class);
