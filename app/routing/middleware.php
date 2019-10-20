1497e0e493eed1d1289798cf670c26dafb042432
Frederic G. Østby <frederic.g.ostby@gmail.com>
2019-10-20 11:43:53 +0200

    Update middleware.php

--- app/routing/middleware.php
+++ app/routing/middleware.php
@@ -2,8 +2,10 @@
 
 use mako\http\routing\middleware\ContentSecurityPolicy;
 use mako\http\routing\middleware\SecurityHeaders;
+use mako\validator\input\http\routing\middleware\InputValidation;
 
 /** @var \mako\http\routing\Dispatcher $dispatcher */
 
 //$dispatcher->registerMiddleware('content_security_policy', ContentSecurityPolicy::class);
 //$dispatcher->registerMiddleware('security_headers', SecurityHeaders::class);
+//$dispatcher->registerMiddleware('input_validation', InputValidation::class);
