25834e7709def35cae17ab263b480f927ee74950
Frederic G. Østby <frederic.g.ostby@gmail.com>
2020-07-12 20:39:01 +0200

    Update redis.php

--- app/config/redis.php
+++ app/config/redis.php
@@ -20,12 +20,14 @@ return
 	 *
 	 * host              : IP address or hostname of the Redis server.
 	 * port              : Port of the Redis server.
-	 * password          : (optional) Password of the Redis server.
-	 * database          : (optional) Database index to use for the connection.
+	 * username          : (optional - default null) Redis username.
+	 * password          : (optional - default null) Redis password.
+	 * database          : (optional - default 0) Database index to use for the connection.
 	 * persistent        : (optional - default false) Set to true to make the connection persistent.
 	 * timeout           : (optional - default 60) Read/write operations timeout in seconds.
 	 * connection_timeout: (optional - default 5) Connection timeout in seconds.
 	 * tcp_nodelay       : (optional - default true) Disable Nagle's algorithm for reduced latency.
+	 * resp              : (optional - default 2) RESP version.
 	 */
 	'configurations' =>
 	[
