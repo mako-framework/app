3e9cb9a1e34ab9643b787fe356214cd1246fc1d8
n0nag0n <n0nag0n@users.noreply.github.com>
2023-05-15 08:03:22 -0600

    Added inherited variables as comments (#11)
    It'll help with context :)

--- app/routing/routes.php
+++ app/routing/routes.php
@@ -2,5 +2,7 @@
 
 use app\controllers\Index;
 
-/** @var \mako\http\routing\Routes $routes */
+/** @var \mako\http\routing\Routes     $routes    */
+/** @var \mako\application\Application $app       */
+/** @var \mako\syringe\Container       $container */
 $routes->get('/', [Index::class, 'welcome']);
